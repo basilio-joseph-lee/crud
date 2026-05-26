0b53ffb964c6a3cddc4577aea4aaec69c7358cea
basilio-joseph-lee <manansalajosephlee@gmail.com>
2026-05-26 18:44:42 +0800

    update avatar_url to ImageField, update CORS

--- config/settings.py
+++ config/settings.py
@@ -157,8 +157,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
-
+CORS_ALLOWED_ORIGINS = [
+    "https://aurumgrand.vercel.app",
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
